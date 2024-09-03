359a09cd7b370a2d20915f9e4a1d9257587748f1
thienphanexcalibur <thienkphan@gmail.com>
2024-09-03 21:56:30 +0700

    WIP

---
+++ packages/demo/theme.config.jsx
@@ -0,0 +1,11 @@
+export default {
+  logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  nextThemes: {
+    defaultTheme: "system",
+  },
+  primarySaturation: 0,
+  primaryHue: 0
+};
